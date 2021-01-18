5704fab4a493d2c32ad10cfc6f5a9237dc310146
zqq <1716145674@qq.com>
2021-01-19 00:06:21 +0800

    谷粒商城第一天:搭框架(pms为例)

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/AttrMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.AttrEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品属性
+ * 
+ * @author zqq
+ * @email zqq@atguigu.com
+ * @date 2021-01-18 21:05:27
+ */
+@Mapper
+public interface AttrMapper extends BaseMapper<AttrEntity> {
+	
+}
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/BrandService.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.pms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.common.bean.PageParamVo;
+import com.atguigu.gmall.pms.entity.BrandEntity;
+
+import java.util.Map;
+
+/**
+ * 品牌
+ *
+ * @author zqq
+ * @email zqq@atguigu.com
+ * @date 2021-01-18 21:05:27
+ */
+public interface BrandService extends IService<BrandEntity> {
+
+    PageResultVo queryPage(PageParamVo paramVo);
+}
+
